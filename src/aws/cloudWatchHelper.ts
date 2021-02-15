3401ff1da0d53c7f11e511777f6cd25eb4636038
Ajinkya <11447401+ajinkya599@users.noreply.github.com>
2021-02-15 23:26:01 +0530

    Add region

--- src/aws/cloudWatchHelper.ts
+++ src/aws/cloudWatchHelper.ts
@@ -4,7 +4,8 @@ import { CloudWatchLogs } from 'aws-sdk';
 export function publishLogEvent(logGroup: string, logStream: string, eventMessage: string) {
   const accessKeyId = core.getInput('access-key-id', { required: true });
   const secretAccessKey = core.getInput('secret-access-key', { required: true });
-  let cloudwatchlogs = new CloudWatchLogs({ accessKeyId: accessKeyId, secretAccessKey: secretAccessKey });
+  const region = core.getInput('region', { required: true });
+  let cloudwatchlogs = new CloudWatchLogs({ region: region, accessKeyId: accessKeyId, secretAccessKey: secretAccessKey });
 
   let eventParams = {
     logEvents: [
@@ -17,7 +18,7 @@ export function publishLogEvent(logGroup: string, logStream: string, eventMessag
     logStreamName: logStream
   };
 
-  console.log(`Publishing the event to cloudwatch. Log group: ${logGroup}, Log stream: ${logStream}\nEvent params: ${eventParams}`);
+  console.log(`Publishing the event to cloudwatch. Log group: ${logGroup}, Log stream: ${logStream}\nEvent params: ${JSON.stringify(eventParams)}`);
   cloudwatchlogs.putLogEvents(eventParams, function (err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else console.log(data);           // successful response
