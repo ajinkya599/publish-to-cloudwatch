08a592569fa44cb185cd9a2e6c1c7bf6cdabcac3
Ajinkya <11447401+ajinkya599@users.noreply.github.com>
2021-02-15 23:13:30 +0530

    Added action implementation

---
+++ src/aws/cloudWatchHelper.ts
@@ -0,0 +1,32 @@
+import * as core from '@actions/core';
+import { CloudWatchLogs } from 'aws-sdk';
+
+export function publishLogEvent(logGroup: string, logStream: string, eventMessage: string) {
+  const accessKeyId = core.getInput('access-key-id', { required: true });
+  const secretAccessKey = core.getInput('secret-access-key', { required: true });
+  let cloudwatchlogs = new CloudWatchLogs({ accessKeyId: accessKeyId, secretAccessKey: secretAccessKey });
+
+  let eventParams = {
+    logEvents: [
+      {
+        message: eventMessage,
+        timestamp: getTimestamp()
+      }
+    ],
+    logGroupName: logGroup,
+    logStreamName: logStream
+  };
+
+  console.log(`Publishing the event to cloudwatch. Log group: ${logGroup}, Log stream: ${logStream}\nEvent params: ${eventParams}`);
+  cloudwatchlogs.putLogEvents(eventParams, function (err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data);           // successful response
+  });
+
+  console.log('Posted event to cloudwatch!');
+}
+
+function getTimestamp(): number {
+  // Cloudwatch API expects event timestamp to be the no. of milliseconds elapsed since  'Jan 1, 1970 00:00:00 UTC'
+  return new Date().valueOf();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 import * as core from '@actions/core';
+import * as fs from 'fs';
+import * as CloudWatchHelper from './aws/cloudWatchHelper';
 
 async function run() {
   try {
-    const logsPath = core.getInput('logs-path');
-    console.log('Not implemented...');
+    console.log('Publishing traceability...');
+    publishTraceability();
+    console.log('Done!');
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function publishTraceability() {
+  const logsPath = core.getInput('logs-path', { required: true });
+  const logGroup = core.getInput('log-group', { required: true });
+  const logStream = core.getInput('log-stream', { required: true });
+  const eventMessage = JSON.stringify(readJsonFromFile(logsPath));
+  CloudWatchHelper.publishLogEvent(logGroup, logStream, eventMessage);
+}
+
+function readJsonFromFile(path: string): any {
+  try {
+    const rawContent = fs.readFileSync(path, 'utf-8');
+    return JSON.parse(rawContent);
+  } catch(error) {
+    throw new Error(`An error occured while reading/parsing the file: ${path}`);
+  }    
 }
 
 run();
